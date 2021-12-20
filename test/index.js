e640227b033b4b74709369dfbd3486453a7aee87
john <comsamo84@gmail.com>
2021-12-20 15:44:59 +0900

    update

--- test/index.js
+++ test/index.js
@@ -13,6 +13,18 @@ describe('Test suit plus', function () {
 		expect(psp.plus(5, 4)).to.equal(9);
   });
 
+	it('A + 4 = false', function () {
+		expect(psp.plus('A', 4)).to.be.false;
+  });
+
+	it('3 + 3 + 4 = 10', function () {
+		expect(psp.plus(3, 3, 4)).to.equal(10);
+  });
+
+	it('1 + 1 + 1 + 1 = 4', function () {
+		expect(psp.plus(1, 1, 1, 1)).to.equal(4);
+  });
+
 
 });
 
@@ -24,6 +36,18 @@ describe('Test suit minus', function () {
 
 	it('4 - 5 = -1', function () {
 		expect(psp.minus(4, 5)).to.equal(-1);
+  });
+
+	it('A - 4 = false', function () {
+		expect(psp.minus('A', 4)).to.be.false;
+  });
+
+	it('3 - 3 - 4 = -4', function () {
+		expect(psp.minus(3, 3, 4)).to.equal(-4);
+  });
+
+	it('1 - 1 - 1 - 1 = -4', function () {
+		expect(psp.minus(1, 1, 1, 1)).to.equal(-4);
   });
 
 });
