382074b076602e2a7b828f5885b1dbfe64c32bf7
kimsy971222 <kimsy@codecraft.co.kr>
2021-12-20 15:18:11 +0900

    test 작성해보기?

--- test/index.js
+++ test/index.js
@@ -1,7 +1,15 @@
+const Psp = require('../src/index')
+
 const assert = require('chai').assert;
+const expect = require('chai').expect
+
 
 describe('Test suit', function () {
   it('should be ok', function (done) {
     done();
   });
+
+  it("4 - 5 = -1", () => {
+    expect(Psp.minus(4,5)).to.equal(-1);
+  })
 });
