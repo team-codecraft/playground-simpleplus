839946078ddbb6bf823bd6194aa4b959655b28c1
john <comsamo84@gmail.com>
2021-12-20 15:12:16 +0900

    update

--- test/index.js
+++ test/index.js
@@ -1,7 +1,24 @@
-const assert = require('chai').assert;
+const Psp = require('../src/index.js');
+const { expect } = require('chai');
+
 
 describe('Test suit', function () {
-  it('should be ok', function (done) {
-    done();
+	let psp = new Psp();
+
+  it('4 + 5 = 9', function () {
+		expect(psp.plus(4, 5)).to.equal(9);
   });
+
+	it('5 + 4 = 9', function () {
+		expect(psp.plus(5, 4)).to.equal(9);
+  });
+
+	it('5 - 4 = 1', function () {
+		expect(psp.minus(5, 4)).to.equal(1);
+  });
+
+	it('4 - 5 = -1', function () {
+		expect(psp.minus(4, 5)).to.equal(-1);
+  });
+
 });
