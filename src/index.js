c7169a54d111d8c8c6a481736a3a06c251d3cf6c
kimsy971222 <kimsy@codecraft.co.kr>
2021-12-20 14:54:25 +0900

    minus 함수 작성

--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@ function Psp() {
 
 	}
 
-	Psp.prototype.minus = function () {
+	Psp.prototype.minus = function (a,b) {
+		var c,d;
+		
+		switch(typeof a) {
+			case 'string':
+				c = Number(a);
+				break;
+			case 'number':
+				c = a;
+				break;
+		}
 
+		switch(typeof b) {
+			case 'string':
+				d = Number(b);
+				break;
+			case 'number':
+				d = a;
+				break;
+		}
+
+		if (c === undefined || d === undefined) {
+			return null;
+		}
+
+		return c-d;
+		
 	}
 }
 module.exports = Psp;
