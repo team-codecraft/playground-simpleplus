246941251dc8745221e4ccdd6cd6bd81ec928df3
sangwon <sangwon@sangwonui-iMac.local>
2021-12-20 15:46:41 +0900

    plus

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function Psp() {
-	Psp.prototype.plus = function (a, b) {
-		return a + b;
+	Psp.prototype.plus = function (...args) {
+		const result = args.reduce((acc, cur) => acc + cur);
+		return typeof result === 'number' ? result : false;
 	}
 
 	Psp.prototype.minus = function () {
